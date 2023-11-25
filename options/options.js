af9adcb99eec4bf1c9d7aae71425f4aeda246e25
Crowdbotics <team@crowdbotics.com>
2023-11-25 00:13:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wecarecommunityplac-44745",
+  url: "https://wecarecommunityplac-44745.botics.co",
+  api: "https://wecarecommunityplac-44745.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
